5d35b42cbc70deaa4b5e232963fd02bf5542f8db
Daniel P. Mgawi <pdmgawi@gmail.com>
2023-12-22 13:43:41 +0200

    Added New Root Layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,41 @@
 <script>
-  import "../app.css";
+  import SideNav from "$lib/components/sideNav.svelte";
+import "../app.css";
 </script>
 
 <svelte:head>
   <script async defer src="https://www.google.com/recaptcha/api.js?render={import.meta.env.VITE_SITEKEY}">
   </script>
 </svelte:head>
-<slot />
+
+<div class="container">
+
+<SideNav></SideNav>
+<section><slot /></section>
+
+</div>
+
+<style>
+  .container{
+    display: flex;
+    height: 100vh;
+    width: 100vw;
+  }
+  
+  aside {
+    background-color: grey;
+    width: 25%;
+    display: flex;
+    flex-direction: column;
+  }
+
+  .nav-link {
+    padding: 1rem;
+
+  }
+
+  
+  section {
+    width: 100%;
+  }
+</style>
