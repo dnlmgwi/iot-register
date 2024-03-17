b602663ffd8ae590b492a6aae574e6c4e2a6f9fc
Daniel P. Mgawi <pdmgawi@gmail.com>
2024-03-17 11:18:07 +0200

    Delete GetTotalAttendanceAndGroupByMonth.ts

--- src/lib/useCases/GetTotalAttendanceAndGroupByMonth.ts
+++
@@ -1,9 +0,0 @@
-import type { UserProfileRepository } from '$lib/repositories/UserProfileRepository';
-
-export class GetTotalAttendanceAndGroupByDay {
-	constructor(private userProfileRepository: UserProfileRepository) {}
-
-	async execute(userId: string) {
-		return this.userProfileRepository.GetAttendanceAndGroupByDate(userId);
-	}
-}
