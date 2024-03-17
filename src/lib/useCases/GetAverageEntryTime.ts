d6201afc73f2a6268f97e0bc92f6d3668b31517a
Daniel P. Mgawi <pdmgawi@gmail.com>
2024-03-17 11:18:23 +0200

    Create GetAverageEntryTime.ts

---
+++ src/lib/useCases/GetAverageEntryTime.ts
@@ -0,0 +1,9 @@
+import type { UserProfileRepository } from '$lib/repositories/UserProfileRepository';
+
+export class GetAverageEntryTimeUseCase {
+	constructor(private userProfileRepository: UserProfileRepository) {}
+
+	async execute(userId: string) {
+		return this.userProfileRepository.GetAverageEntryTime(userId);
+	}
+}
