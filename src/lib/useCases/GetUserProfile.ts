e3ecb84113e2ef037e0daa7d39a96716d4c10cad
Daniel P. Mgawi <pdmgawi@gmail.com>
2024-04-04 20:33:19 +0200

    v0.0.2
    - Error on Navigation

--- src/lib/useCases/GetUserProfile.ts
+++ src/lib/useCases/GetUserProfile.ts
@@ -5,13 +5,8 @@ export class GetUserProfileUseCase {
 	constructor(private userProfileRepository: UserProfileRepository) {}
 
 	async execute(userId: string): Promise<Profile> {
-		try {
-			const profile = await this.userProfileRepository.getUserProfile(userId);
+		const profile = await this.userProfileRepository.getUserProfile(userId);
 
-			return profile;
-		} catch (error) {
-			// Re-throw the error to be handled by the caller
-			throw new Error('Profile Incomplete');
-		}
+		return profile;
 	}
 }
--- src/lib/useCases/GetUserProfileByID.ts
+++ src/lib/useCases/GetUserProfileByID.ts
@@ -5,13 +5,8 @@ export class GetUserProfileByIDUseCase {
 	constructor(private userProfileRepository: UserProfileRepository) {}
 
 	async execute(userId: string): Promise<Profile> {
-		try {
-			const profile = await this.userProfileRepository.getUserProfileByStudentID(userId);
+		const profile = await this.userProfileRepository.getUserProfileByStudentID(userId);
 
-			return profile;
-		} catch (error) {
-			// Re-throw the error to be handled by the caller
-			throw new Error('Profile Incomplete');
-		}
+		return profile;
 	}
 }
